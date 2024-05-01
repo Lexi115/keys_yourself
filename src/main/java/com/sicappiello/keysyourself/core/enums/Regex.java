ba8b729e594529cc8c8a863ad976813cc49bac9b
CarmeloCapp <cappiello11@gmail.com>
2024-05-01 16:35:23 +0200

    Changes by keite

---
+++ src/main/java/com/sicappiello/keysyourself/core/enums/Regex.java
@@ -0,0 +1,30 @@
+package com.sicappiello.keysyourself.core.enums;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public abstract class Regex {
+    // Regex per utente
+    public static final String USER_EMAIL = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+    public static final String USER_PASSWORD = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%_*#?&])[A-Za-z\\d@$!%_*#?&]{8,}$";
+    public static final String USER_FIRST_NAME = ".{1,200}";
+    public static final String USER_LAST_NAME = ".{1,50}";
+    public static final String USER_ADDRESS = ".{1,100}";
+    public static final String USER_PHONE_NUMBER = "^\\+\\d{2,6}\\s?[\\d\\s]{10,16}$";
+
+    //Regex per giochi
+    public static final String GAME_NAME = ".{1,150}";
+    public static final String GAME_PRICE = "^\\d{1,10}.\\d{2}$";
+    public static final String GAME_AVAILABLE_QUANTITY = "\\d";
+    public static final String GAME_DESCRIPTION = ".{1,2000}";
+    public static final String GAME_PRODUCER = ".{1,50}";
+
+    //Regex per generi
+    public static final String GENRE_NAME = ".{1,30}";
+
+    public static boolean matches(String regex, String input) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.matches();
+    }
+}
