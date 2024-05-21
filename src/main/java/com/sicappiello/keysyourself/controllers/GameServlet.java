6e47273685b435509690248b5fbe53ec633e9d30
cappi <cappiello11@gmail.com>
2024-05-21 16:55:18 +0200

    total e cart vario con altra roba non ricordo idk lmaoooo

--- src/main/java/com/sicappiello/keysyourself/controllers/GameServlet.java
+++ src/main/java/com/sicappiello/keysyourself/controllers/GameServlet.java
@@ -19,6 +19,13 @@ import java.io.IOException;
 public class GameServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/results/game.jsp");
+        GameDAO gameDAO = new GameDAO(Functions.getContextDatabase(this));
+        Game selectedGame = gameDAO.getById(Integer.parseInt(request.getParameter("id")));
+        if(selectedGame!=null){
+            request.setAttribute("game",selectedGame);
+        } else {
+            throw new RuntimeException("no game niga");
+        }
         rd.forward(request,response);
     }
 
