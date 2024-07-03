409757eea9a5dd65ed437aadd1d50d4753ce5fa6
CarmeloCapp <cappiello11@gmail.com>
2024-07-03 17:17:56 +0200

    js esterno

--- src/main/java/com/sicappiello/keysyourself/util/FileSize.java
+++ src/main/java/com/sicappiello/keysyourself/util/FileSize.java
@@ -2,7 +2,7 @@ package com.sicappiello.keysyourself.util;
 
 public abstract class FileSize {
     public static final int MIN = 0;
-    public static final int MAX = 1024 * 1024; // 10 MB
+    public static final int MAX = 1024 * 1024 * 10; // 10 MB
     public static final int MAX_REAL = 1024 * 1024 * 20; // 20 MB
     public static final int REQUEST_MAX = 1024 * 1024 * 20; // 20 MB
     public static final int MAX_MB = MAX / 1024 / 1024;
